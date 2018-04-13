e324c9f0c5390f25277e49103fd052d16c564279
Eviatar Bach <eviatarbach@protonmail.com>
2018-04-13 18:59:53 -0400

    Fixing for gradual coupling runs

--- fortran/gradual_coupling.py
+++ fortran/gradual_coupling.py
@@ -3,7 +3,7 @@ from simulation_runner import run_simulation, Sequential
 import numpy
 
 sim_ids = run_simulation('./maooam_lyap {sim_id}', 'params/params_{sim_id}.nml',
-               template_path='params_template.nml',
-               sweep_parameters={'coupling_motion': numpy.linspace(0.001, 1, 10),
-                                 'coupling_thermo': numpy.linspace(0.001, 1, 10)},
-               naming=Sequential, delay=1, run=False)
+                         template_path='params_template.nml',
+                         sweep_parameters={'coupling_motion': numpy.linspace(0.001, 1, 10),
+                                           'coupling_thermo': numpy.linspace(0.001, 1, 10)},
+                         naming=Sequential(zfill=3), delay=1, run=True)
--- fortran/simulation_runner.py
+++ fortran/simulation_runner.py
@@ -1,56 +1,63 @@
-"""
-By Eviatar Bach, see https://github.com/eviatarbach/simulation_runner
-"""
 import subprocess
 import itertools
 import time
 import math
 import operator
 from functools import reduce
+from abc import ABC, abstractmethod
 
 from mako.template import Template
+import numpy
+import xarray
 
 
-class _Namer:
-    def __init__(self, **kwargs):
+class _Namer(ABC):
+    """
+    Abstract class for assigning simulation IDs to simulation. Only the `next`
+    method has to be implemented.
+    """
+    def start(self, length):
         pass
 
+    @abstractmethod
     def next(self, keys, param_set):
+        """
+        Generate simulation ID for a given parameter set
+        """
         pass
-
-
-class Hasher(_Namer):
-    def next(self, keys, param_set):
-        return hash(''.join(map(str, param_set))) % 65535
-
-
-class String(_Namer):
-    def next(self, keys, param_set):
-        return '_'.join('{key}={param}'.format(key=key, param=param) for key, param in zip(keys, param_set))
 
 
 class Sequential(_Namer):
-    def __init__(self, length):
+    """
+    Name simulations with consecutive numbers and leading zeros
+    """
+    def __init__(self, zfill=None):
+        self.zfill = zfill
+
+    def start(self, length):
         self.count = -1
-        self.zfill = math.floor(math.log10(length - 1) + 1)
+        if self.zfill is None:
+            self.zfill = math.floor(math.log10(length - 1) + 1)
 
     def next(self, keys, param_set):
         self.count += 1
         return str(self.count).zfill(self.zfill)
 
 
-def run_simulation(command, config_path, template_path=None,
+def run_simulation(command, config_path, sweep_id=None, template_path=None,
                    template_text=None, single_parameters={},
-                   sweep_parameters={}, naming=String, build=False, run=True,
-                   verbose=True, delay=False):
-    '''
+                   sweep_parameters={}, naming=Sequential(), build=False,
+                   run=True, verbose=True, delay=False):
+    r"""
     EXAMPLES:
 
-      >>> run_simulation('cat {sim_id}.txt', '{sim_id}.txt', template_text='Hello ${x*10}\n', sweep_parameters={'x': (1, 2, 3)}, verbose=False)
-      Hello 10
-      Hello 20
-      Hello 30
-    '''
+    >>> run_simulation('cat {sim_id}.txt', '{sim_id}.txt',
+    ...                template_text='Hello ${x*10}\n',
+    ...                sweep_parameters={'x': (1, 2, 3)}, verbose=False)
+    Hello 10
+    Hello 20
+    Hello 30
+    """
     if (((template_path is None) and (template_text is None))
         or (not (template_path is None) and not (template_text is None))):
         raise ValueError('Exactly one of `template_path` or `template_text` must be provided.')
@@ -60,9 +67,9 @@ def run_simulation(command, config_path, template_path=None,
     if build:
         pass
 
-    sim_ids = {}
     keys = list(sweep_parameters.keys())
-    values = sweep_parameters.values()
+    values = list(sweep_parameters.values())
+    lengths = [len(value) for value in values]
 
     if sweep_parameters:
         product = itertools.product(*values)
@@ -74,8 +81,9 @@ def run_simulation(command, config_path, template_path=None,
     else:
         config = Template(text=template_text, input_encoding='utf-8')
 
-    name_gen = naming(length=reduce(operator.mul, [len(value) for value in values], 1))
+    naming.start(length=reduce(operator.mul, lengths, 1))
 
+    sim_ids = []
     processes = []
     for param_set in product:
         sweep_params = params.copy()
@@ -83,8 +91,8 @@ def run_simulation(command, config_path, template_path=None,
             for index, value in enumerate(param_set):
                 sweep_params[keys[index]] = value
 
-        sim_id = name_gen.next(keys, sweep_params.values())
-        sim_ids[sim_id] = sweep_params
+        sim_id = naming.next(keys, sweep_params.values())
+        sim_ids.append(sim_id)
 
         config_rendered = config.render_unicode(**sweep_params).encode('utf-8', 'replace')
         config_file = open(config_path.format(sim_id=sim_id), 'wb')
@@ -103,4 +111,10 @@ def run_simulation(command, config_path, template_path=None,
     for process in processes:
         process.wait()
 
-    return sim_ids
+    sim_ids_array = xarray.DataArray(numpy.reshape(numpy.array(sim_ids),
+                                                   lengths),
+                                     coords=values, dims=keys)
+
+    sim_ids_array.to_netcdf('sim_ids{sweep_id}.nc'.format(sweep_id='_' + sweep_id if sweep_id else ''))
+
+    return sim_ids_array
