6d63f2e75d004643bdc3e08ec4c47211432bfc6f
Eviatar Bach <eviatarbach@protonmail.com>
2018-04-14 14:48:53 -0400

    More plots

--- fortran/plot_gradual_coupling.py
+++ fortran/plot_gradual_coupling.py
@@ -2,53 +2,101 @@ import matplotlib.pyplot as plt
 import xarray
 import numpy
 
-dat = xarray.DataArray(numpy.reshape(range(100), (10, 10)),
-                       coords=[numpy.linspace(0.001, 1, 10),
-                               numpy.linspace(0.001, 1, 10)],
-                       dims=['coupling_motion', 'coupling_thermo'])
+dat = xarray.open_dataarray('sim_ids.nc')
 
 # Full motion coupling, gradual thermo
 sim_ids = dat[9, :].data
 thermo = dat[9, :]['coupling_thermo'].data
 
 for i, sim_id in enumerate(sim_ids):
-    lyap = open('mean_lyapunov_{sim_id}.dat'.format(sim_id=str(sim_id).zfill(3))).read()
+    lyap = open('mean_lyapunov_{sim_id}.dat'.format(sim_id=sim_id)).read()
     lyap = numpy.array(lyap.split('\n')[0].split()[1:], dtype='float')
     plt.scatter(numpy.array([thermo[i]]*36) + numpy.random.rand(36)/30, lyap, c='black', s=5)
 
-plt.ylim(-0.5, 0.5)
+plt.ylim(-0.2, 0.2)
 plt.xlabel('Thermodynamic coupling')
 plt.ylabel('Lyapunov exponent (days$^{-1}$)')
 plt.savefig('gradual_thermo.png')
+plt.clf()
 
 # Full thermo coupling, gradual motion
 sim_ids = dat[:, 9].data
 thermo = dat[:, 9]['coupling_motion'].data
 
 for i, sim_id in enumerate(sim_ids):
-    lyap = open('mean_lyapunov_{sim_id}.dat'.format(sim_id=str(sim_id).zfill(3))).read()
+    lyap = open('mean_lyapunov_{sim_id}.dat'.format(sim_id=sim_id)).read()
     lyap = numpy.array(lyap.split('\n')[0].split()[1:], dtype='float')
     plt.scatter(numpy.array([thermo[i]]*36) + numpy.random.rand(36)/30, lyap, c='black', s=5)
 
-
-plt.ylim(-0.5, 0.5)
+plt.ylim(-0.2, 0.2)
 plt.xlabel('Motion coupling')
 plt.ylabel('Lyapunov exponent (days$^{-1}$)')
 plt.savefig('gradual_motion.png')
+plt.clf()
 
-mat = numpy.zeros((10, 10))
+# Gradual thermo, gradual motion
+sim_ids = numpy.diag(dat)
+thermo = dat[:, 9]['coupling_motion'].data
+
+for i, sim_id in enumerate(sim_ids):
+    lyap = open('mean_lyapunov_{sim_id}.dat'.format(sim_id=sim_id)).read()
+    lyap = numpy.array(lyap.split('\n')[0].split()[1:], dtype='float')
+    plt.scatter(numpy.array([thermo[i]]*36) + numpy.random.rand(36)/30, lyap, c='black', s=5)
+
+plt.ylim(-0.2, 0.2)
+plt.xlabel('Coupling')
+plt.ylabel('Lyapunov exponent (days$^{-1}$)')
+plt.savefig('gradual_both.png')
+plt.clf()
+
+# Low thermo, gradual motion
+sim_ids = dat[:, 0].data
+thermo = dat[:, 0]['coupling_motion'].data
+
+for i, sim_id in enumerate(sim_ids):
+    lyap = open('mean_lyapunov_{sim_id}.dat'.format(sim_id=sim_id)).read()
+    lyap = numpy.array(lyap.split('\n')[0].split()[1:], dtype='float')
+    plt.scatter(numpy.array([thermo[i]]*36) + numpy.random.rand(36)/30, lyap, c='black', s=5)
+
+plt.ylim(-0.2, 0.2)
+plt.xlabel('Motion coupling')
+plt.ylabel('Lyapunov exponent (days$^{-1}$)')
+plt.savefig('gradual_low_thermo.png')
+plt.clf()
+
+# Low motion, gradual thermo
+sim_ids = dat[0, :].data
+thermo = dat[0, :]['coupling_thermo'].data
+
+for i, sim_id in enumerate(sim_ids):
+    lyap = open('mean_lyapunov_{sim_id}.dat'.format(sim_id=sim_id)).read()
+    lyap = numpy.array(lyap.split('\n')[0].split()[1:], dtype='float')
+    plt.scatter(numpy.array([thermo[i]]*36) + numpy.random.rand(36)/30, lyap, c='black', s=5)
+
+plt.ylim(-0.2, 0.2)
+plt.xlabel('Thermodynamic coupling')
+plt.ylabel('Lyapunov exponent (days$^{-1}$)')
+plt.savefig('gradual_low_motion.png')
+plt.clf()
+
+# Plot number of positive LEs
+mat = numpy.zeros((10, 10), dtype='int')
 for i in range(10):
     for j in range(10):
         sim_id = dat[i, j].data
-        lyap = open('mean_lyapunov_{sim_id}.dat'.format(sim_id=str(sim_id).zfill(3))).read()
+        lyap = open('mean_lyapunov_{sim_id}.dat'.format(sim_id=sim_id)).read()
         lyap = numpy.array(lyap.split('\n')[0].split()[1:], dtype='float')
         mat[i, j] = sum(lyap > 0)
 
-plt.matshow(mat)
-plt.xticks(range(10), numpy.linspace(0.001, 1, 10))
-plt.yticks(range(10), numpy.linspace(0.001, 1, 10))
-plt.colorbar()
+N = mat.max() - mat.min() + 1
+plt.matshow(mat, fignum=1, origin='lower', cmap=plt.cm.get_cmap('viridis', N))
+ax = plt.gca()
+ax.xaxis.tick_bottom()
+plt.xticks(range(10), numpy.around(numpy.linspace(0.001, 1, 10), decimals=2))
+plt.yticks(range(10), numpy.around(numpy.linspace(0.001, 1, 10), decimals=2))
+plt.colorbar(ticks=range(mat.min(), mat.max() + 1))
+plt.clim(mat.min() - 0.5, mat.max() + 0.5)
 plt.xlabel('Motion coupling')
 plt.ylabel('Thermodynamic coupling')
 plt.title('Number of positive Lyapunov exponents')
-plt.savefig('gradual_both.png', bbox_inches='tight')
+plt.savefig('gradual_grid.png', bbox_inches='tight')
